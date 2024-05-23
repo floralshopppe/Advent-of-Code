ba62844f33811f94cf0a41ef71cd64e942fc4ac5
Alex <stefanalecs003@yahoo.com>
2024-05-23 11:14:08 +0300

    Add files via upload

--- 2015/Python/6.py
+++ 2015/Python/6.py
@@ -1,21 +1,27 @@
-Row = 1000
-Column = 1000
+Rows = 1000
+Columns = 1000
  
+f = open("2015\Python\input.txt", "r")
+for x in f:
+  print(x)
+
 # Initialize matrix
 matrix = []
 print("Enter the entries row wise:")
  
 # For user input
 # A for loop for row entries
-for row in range(Row):    
+for row in range(Rows):    
     a = []
     # A for loop for column entries
-    for column in range(Column):   
-        a.append(int(input()))
+    for column in range(Columns):   
+        a.append(0)
     matrix.append(a)
  
 # For printing the matrix
-for row in range(Row):
-    for column in range(Column):
+for row in range(Rows):
+    for column in range(Columns):
         print(matrix[row][column], end=" ")
     print()
+
+f.close()
